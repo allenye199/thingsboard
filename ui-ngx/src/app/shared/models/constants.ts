bdc16d5aa314f91bfc24ee95a767622d0e66a628
allenye199 <allenye888@gmail.com>
2025-05-07 10:48:22 +0800

    修改帮助链接 http://thingsboard.io 到http://www.goalthink.net

--- ui-ngx/src/app/shared/models/constants.ts
+++ ui-ngx/src/app/shared/models/constants.ts
@@ -79,7 +79,7 @@ export const resolveBreakpoint = (breakpoint: string): string => {
   return breakpoint;
 };
 
-export const helpBaseUrl = 'https://thingsboard.io';
+export const helpBaseUrl = 'http://www.goalthink.net';
 
 export const docPlatformPrefix = '';
 
